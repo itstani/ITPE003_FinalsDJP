7c20597d44b8ba704b27cd4aa0e24b4a8c48f402
rbdej <rbdjramildejesus@gmail.com>
2024-07-16 15:00:08 +0800

    javascript test, add to inventory button decrements stockQuantity by 1 when clicked

--- javascriptFile.js
+++ javascriptFile.js
@@ -1,10 +1,12 @@
-function onLoadSetValues() {
-//set initial value of item prices
-priceDice = 100;
-priceMinifigure = 200;
+let stockQuantity = 5; //set stock value to 5
 
-//set initial stock amount of all items
-stockQuantity = 5;
-document.getElementById("qty").innerHTML = stockQuantity;
+function onLoadSetValues() {
+    document.getElementById("qty").innerHTML = stockQuantity; //set whatever stock value to be printed in the website when loaded
 }
 
+function AddToInventoryButton() {
+    currentStock = stockQuantity; //retrieve stock amount and sets it as current stock
+    currentStock -= 1; //update stock amount, remove 1 per button press
+    stockQuantity = currentStock; //save new stock amount to the stockQuantity variable
+    document.getElementById("qty").innerHTML = currentStock; //update the html displaying the stock amount
+}

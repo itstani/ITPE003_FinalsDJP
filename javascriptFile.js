61963e5bb34154a3b944835cb7046ddb427a75c8
rbdej <rbdjramildejesus@gmail.com>
2024-07-16 08:44:19 +0800

    javascript test

---
+++ javascriptFile.js
@@ -0,0 +1,10 @@
+function onLoadSetValues() {
+//set initial value of item prices
+priceDice = 100;
+priceMinifigure = 200;
+
+//set initial stock amount of all items
+stockQuantity = 5;
+document.getElementById("qty").innerHTML = stockQuantity;
+}
+
